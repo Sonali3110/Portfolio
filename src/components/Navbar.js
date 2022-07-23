4afbeb83e831430a1dd4c97407af163dba1153d0
Sonali Raj <sonaliraj.110602@gmail.com>
2022-07-23 14:36:33 +0530

    donw with changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,7 +32,7 @@ export default function Navbar(props) {
 //  Navbar.propTypes={
 //   title:PropTypes.string.isRequired,
 //    aboutText:PropTypes.string}
-
+//bk
              
                 
 //WHEN NO PROPS ARE PASSED , THESE ARE SET AS DEFALUT VALUES.
